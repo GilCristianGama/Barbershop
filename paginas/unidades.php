728cdf642cfe337e944f2e49292c8dc7649dd234
GilCristianGama <ppgama50@gmail.com>
2020-10-15 11:53:45 -0300

    arquivobase

--- paginas/unidades.php
+++ paginas/unidades.php
@@ -9,7 +9,7 @@
 <div class="card" style="width: 18rem;">
     <img src="http://placehold.it/300x300" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Igarassu</h5>
+      <h5 class="card-title">Igarass</h5>
       <p class="card-text">Modelo de Corte Masculino Stylo...</p>
       <a href="#" class="btn btn-primary stretched-link">Agendar</a>
     </div>
